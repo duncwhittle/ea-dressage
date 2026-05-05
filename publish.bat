9b4d0a7b24395765c9c3ce8697ac9db12cad42ee
Dunc Whittle <duncwhittle@gmail.com>
2026-05-05 21:10:05 +1000

    fix index.html

--- publish.bat
+++ publish.bat
@@ -1,4 +1,4 @@
-cp ea_dressage_v14.html index.html
+copy ea_dressage_v14.html index.html
 git add .
-git commit -m "update index from v14"
+git commit -m 'update index from v14'
 git push origin master
